af350ace792d15ceb9aae5fd6ed73a5ae4415833
Deepa <dipali.jadhav@proteustech.in>
2022-11-28 18:04:18 +0530

    modal and player

--- Js/app.js
+++ Js/app.js
@@ -266,39 +266,39 @@ function validateInput() {
 }
 // ==========Email validation================
 // ==========text area validation===========
-$('textarea').keyup(function () {
+// $('textarea').keyup(function () {
 
-  var characterCount = $(this).val().length,
-    current = $('#current'),
-    maximum = $('#maximum'),
-    theCount = $('#the-count');
+//   var characterCount = $(this).val().length,
+//     current = $('#current'),
+//     maximum = $('#maximum'),
+//     theCount = $('#the-count');
 
-  current.text(characterCount);
-  if (characterCount < 70) {
-    current.css('color', '#666');
-  }
-  if (characterCount > 70 && characterCount < 90) {
-    current.css('color', '#6d5555');
-  }
-  if (characterCount > 90 && characterCount < 100) {
-    current.css('color', '#793535');
-  }
-  if (characterCount > 100 && characterCount < 120) {
-    current.css('color', '#841c1c');
-  }
-  if (characterCount > 120 && characterCount < 139) {
-    current.css('color', '#8f0001');
-  }
+//   current.text(characterCount);
+//   if (characterCount < 70) {
+//     current.css('color', '#666');
+//   }
+//   if (characterCount > 70 && characterCount < 90) {
+//     current.css('color', '#6d5555');
+//   }
+//   if (characterCount > 90 && characterCount < 100) {
+//     current.css('color', '#793535');
+//   }
+//   if (characterCount > 100 && characterCount < 120) {
+//     current.css('color', '#841c1c');
+//   }
+//   if (characterCount > 120 && characterCount < 139) {
+//     current.css('color', '#8f0001');
+//   }
 
-  if (characterCount >= 140) {
-    maximum.css('color', '#8f0001');
-    current.css('color', '#8f0001');
-    theCount.css('font-weight', 'bold');
-  } else {
-    maximum.css('color', '#666');
-    theCount.css('font-weight', 'normal');
-  }
-});
+//   if (characterCount >= 140) {
+//     maximum.css('color', '#8f0001');
+//     current.css('color', '#8f0001');
+//     theCount.css('font-weight', 'bold');
+//   } else {
+//     maximum.css('color', '#666');
+//     theCount.css('font-weight', 'normal');
+//   }
+// });
 
 //==============Dashboard js=============
 // Buttons
@@ -537,3 +537,8 @@ var inputHandler = function (event) {
 TextInput1.addEventListener('input', inputHandler);
 
 alert("success"); // FN
+
+
+// ========Carousal starts=======
+
+// =======Carousal end=============
