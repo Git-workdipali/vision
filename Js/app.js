788ae13bf7ea452a012f13fca46d60d3baf5edc5
Deepa <dipali.jadhav@proteustech.in>
2023-01-03 18:16:36 +0530

    updated

--- Js/app.js
+++ Js/app.js
@@ -34,15 +34,11 @@ function hideOptions() {
 }
 
 function hideContent() {
-  // var x = document.getElementById("hidecol-1");
-  // if (x.style.display === "none") {
-  //   x.style.display = "";
-  // } else {
-  //   x.style.display = "none";
-  // }
+
 }
-//Full screen end
-// Collapsible
+//==========Full screen end=========
+
+//============Collapsible===========
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -57,8 +53,9 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-// Collapsible end
-// TreeView start
+//============Collapsible end==========
+
+//===========TreeView start=============
 var toggler = document.getElementsByClassName("caret");
 var i;
 
@@ -68,202 +65,6 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
-
-// Treeview end
-// =========Ripples=========
-// function findRippleOrigin(clickEv) {
-//   const buttonRect = clickEv.currentTarget.getBoundingClientRect();
-//   const x = clickEv.clientX - buttonRect.x;
-//   const y = clickEv.clientY - buttonRect.y;
-//   return { x, y };
-// }
-
-// const button = document.querySelector(".btn-ripple");
-
-// button.addEventListener("click", ev => {
-//   const rippleEl = document.createElement("span");
-
-//   const rippleOrigin = findRippleOrigin(ev);
-//   rippleEl.style.top = `${rippleOrigin.y}px`;
-//   rippleEl.style.left = `${rippleOrigin.x}px`;
-
-//   rippleEl.classList.add("ripple");
-
-//   rippleEl.addEventListener("animationend", ev => {
-//     ev.target.remove();
-//   });
-
-//   button.appendChild(rippleEl);
-// });
-
-// ========Ripples End======
-// ========Calender===========
-// var mesos = [
-//   'January',
-//   'February',
-//   'March',
-//   'April',
-//   'May',
-//   'June',
-//   'July',
-//   'August',
-//   'September',
-//   'October',
-//   'November',
-//   'December'
-// ];
-
-// var dies = [
-//   'Sunday',
-//   'Monday',
-//   'Tuesday',
-//   'Wednesday',
-//   'Thursday',
-//   'Friday',
-//   'Saturday'
-// ];
-
-// var dies_abr = [
-//   'Sun',
-//   'Mon',
-//   'Tue',
-//   'Wed',
-//   'Thu',
-//   'Fri',
-//   'Sat'
-// ];
-
-// Number.prototype.pad = function (num) {
-//   var str = '';
-//   for (var i = 0; i < (num - this.toString().length); i++)
-//     str += '0';
-//   return str += this.toString();
-// }
-
-// function calendari(widget, data) {
-
-//   var original = widget.getElementsByClassName('actiu')[0];
-
-//   if (typeof original === 'undefined') {
-//     original = document.createElement('table');
-//     original.setAttribute('data-actual',
-//       data.getFullYear() + '/' +
-//       data.getMonth().pad(2) + '/' +
-//       data.getDate().pad(2))
-//     widget.appendChild(original);
-//   }
-
-//   var diff = data - new Date(original.getAttribute('data-actual'));
-
-//   diff = new Date(diff).getMonth();
-
-//   var e = document.createElement('table');
-
-//   e.className = diff === 0 ? 'amagat-esquerra' : 'amagat-dreta';
-//   e.innerHTML = '';
-
-//   widget.appendChild(e);
-
-//   e.setAttribute('data-actual',
-//     data.getFullYear() + '/' +
-//     data.getMonth().pad(2) + '/' +
-//     data.getDate().pad(2))
-
-//   var fila = document.createElement('tr');
-//   var titol = document.createElement('th');
-//   titol.setAttribute('colspan', 7);
-
-//   var boto_prev = document.createElement('button');
-//   boto_prev.className = 'boto-prev';
-//   boto_prev.innerHTML = '&#9666;';
-
-//   var boto_next = document.createElement('button');
-//   boto_next.className = 'boto-next';
-//   boto_next.innerHTML = '&#9656;';
-
-//   titol.appendChild(boto_prev);
-//   titol.appendChild(document.createElement('span')).innerHTML =
-//     mesos[data.getMonth()] + '<span class="any">' + data.getFullYear() + '</span>';
-
-//   titol.appendChild(boto_next);
-
-//   boto_prev.onclick = function () {
-//     data.setMonth(data.getMonth() - 1);
-//     calendari(widget, data);
-//   };
-
-//   boto_next.onclick = function () {
-//     data.setMonth(data.getMonth() + 1);
-//     calendari(widget, data);
-//   };
-
-//   fila.appendChild(titol);
-//   e.appendChild(fila);
-
-//   fila = document.createElement('tr');
-
-//   for (var i = 1; i < 7; i++) {
-//     fila.innerHTML += '<th>' + dies_abr[i] + '</th>';
-//   }
-
-//   fila.innerHTML += '<th>' + dies_abr[0] + '</th>';
-//   e.appendChild(fila);
-
-//   /* Obtinc el dia que va acabar el mes anterior */
-//   var inici_mes =
-//     new Date(data.getFullYear(), data.getMonth(), -1).getDay();
-
-//   var actual = new Date(data.getFullYear(),
-//     data.getMonth(),
-//     -inici_mes);
-
-//   /* 6 setmanes per cobrir totes les posiblitats
-//    *  Quedaria mes consistent alhora de mostrar molts mesos 
-//    *  en una quadricula */
-//   for (var s = 0; s < 6; s++) {
-//     var fila = document.createElement('tr');
-
-//     for (var d = 1; d < 8; d++) {
-//       var cela = document.createElement('td');
-//       var span = document.createElement('span');
-
-//       cela.appendChild(span);
-
-//       span.innerHTML = actual.getDate();
-
-//       if (actual.getMonth() !== data.getMonth())
-//         cela.className = 'fora';
-
-//       /* Si es avui el decorem */
-//       if (data.getDate() == actual.getDate() &&
-//         data.getMonth() == actual.getMonth())
-//         cela.className = 'avui';
-
-//       actual.setDate(actual.getDate() + 1);
-//       fila.appendChild(cela);
-//     }
-
-//     e.appendChild(fila);
-//   }
-
-//   setTimeout(function () {
-//     e.className = 'actiu';
-//     original.className +=
-//       diff === 0 ? ' amagat-dreta' : ' amagat-esquerra';
-//   }, 20);
-
-//   original.className = 'inactiu';
-
-//   setTimeout(function () {
-//     var inactius = document.getElementsByClassName('inactiu');
-//     for (var i = 0; i < inactius.length; i++)
-//       widget.removeChild(inactius[i]);
-//   }, 1000);
-
-// }
-
-// calendari(document.getElementById('calendari'), new Date());
-
 
 //==========Hamburger css start==========
 function openNav() {
@@ -293,7 +94,8 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 }
-// search scope popup pannel end
+//=========search scope popup pannel end=========
+
 //=========toggle starts==========
 var show = function (t) {
   t.style.display = "block"
@@ -311,7 +113,8 @@ document.addEventListener("click", function (t) {
     e && toggle(e)
   }
 }, !1);
-// Toggle end
+//==========Toggle end===========
+
 /*===========hide content and show on click=========*/
 function toggleText() {
   var x = document.getElementById("Myid");
@@ -339,13 +142,12 @@ function hideUser() {
     x.style.display = "block";
   }
 }
-// Hide And show content End
-// LTR
+//===============Hide And show content End===============
+//============LTR============
 function myLtr() {
   var element = document.getElementById("LTR");
   element.classList.toggle("ltr");
 }
-// RTL
 
 //=============dropdown button for col-3-main============
 [".collapsed-menu"].forEach((val) => {
@@ -690,32 +492,40 @@ var RadialProgress = function (size, barSize, barColor, backgroundColor, textCol
     requestAnimationFrame(step);
   };
 };
+// ==================Linegraph=====================
+var trace1 = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [40, 50, 60, 20, -10, 20],
+  name: 'yaxis data',
+  type: 'scatter'
+};
 
-// // ===========name initials changes as per value===========
-// var inputname1 = document.getElementById("TextInput1").value;
+var trace2 = {
+  x: [1, 2, 3, 4, 5, 6, 7],
+  y: [4, 5, 6, 9, 3, 2, 4],
+  name: 'yaxis2 data',
+  yaxis: 'y2',
+  type: 'scatter'
+};
 
-// var getInitials = function (name) {
-//   let initials = name.split(' ');
-//   let charCount = name.length;
+var data = [trace1, trace2];
 
-//   if (initials.length > 1) {
-//     initials = initials.shift().charAt(0) + initials.pop().charAt(0);
-//   } else if (charCount < 1) {
-//     initials = "na";
-//   } else {
-//     initials = name.substring(0, 2);
-//   }
-//   return initials.toUpperCase();
-// }
+var layout = {
+  title: 'Double Y Axis Example',
+  font: { size: 14 },
+  yaxis: { title: 'yaxis title' },
+  yaxis2: {
+    title: 'yaxis2 title',
+    titlefont: { color: 'rgb(148, 103, 189)' },
+    tickfont: { color: 'rgb(148, 103, 189)' },
+    overlaying: 'y',
+    side: 'right'
+  }
+};
 
-// myspan1.innerHTML = getInitials(inputname1);
+var config = { responsive: true }
 
-// var inputHandler = function (event) {
-//   myspan1.innerHTML = getInitials(event.target.value);
-// }
 
-// TextInput1.addEventListener('input', inputHandler);
-
-// alert("success"); // FN
+Plotly.newPlot('myDiv', data, layout, config);
 
 
